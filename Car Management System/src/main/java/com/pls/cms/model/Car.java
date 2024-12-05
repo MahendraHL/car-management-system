7429e8c9753429e00084904efc0bdd90af36f858
Mahendra H L. <mahendra.hl@excelindia.com>
2024-12-05 15:38:45 +0530

    car class is created in model package

---
+++ Car Management System/src/main/java/com/pls/cms/model/Car.java
@@ -0,0 +1,72 @@
+package com.pls.cms.model;
+
+import java.io.Serializable;
+
+public class Car implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    private String carId;
+    private String carName;
+    private String carType;
+    private String description;
+    private String brand;
+    private int model;
+    private String price;
+
+    public String getCarId() {
+        return carId;
+    }
+
+    public void setCarId(String carId) {
+        this.carId = carId;
+    }
+
+    public String getCarName() {
+        return carName;
+    }
+
+    public void setCarName(String carName) {
+        this.carName = carName;
+    }
+
+    public String getCarType() {
+        return carType;
+    }
+
+    public void setCarType(String carType) {
+        this.carType = carType;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public int getModel() {
+        return model;
+    }
+
+    public void setModel(int model) {
+        this.model = model;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+}
