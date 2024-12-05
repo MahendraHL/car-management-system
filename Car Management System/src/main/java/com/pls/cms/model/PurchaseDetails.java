918493b6883ec6a6778e04c23a62b1aebcdfd4c5
Dhanush M N. <dhanush.mn@excelindia.com>
2024-12-05 15:43:31 +0530

    model of PD class is created

---
+++ Car Management System/src/main/java/com/pls/cms/model/PurchaseDetails.java
@@ -0,0 +1,55 @@
+package com.pls.cms.model;
+
+
+import java.io.Serializable;
+
+public class PurchaseDetails implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String contact;
+    private String address;
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
--- Car Management System/src/main/java/com/pls/cms/model/User.java
+++ Car Management System/src/main/java/com/pls/cms/model/User.java
@@ -1,4 +1,5 @@
 package com.pls.cms.model;
 
 public class User {
+
 }
